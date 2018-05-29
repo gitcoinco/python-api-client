f6c7ab4b622bbf523c30758200c9ea23c5ce58ae
Raphael Huefner <raphaelhuefner@gmail.com>
2018-05-29 11:54:07 +0200

    add docstrings to validation

--- gitcoin/validation.py
+++ gitcoin/validation.py
@@ -1,3 +1,4 @@
+"""Validate parameter values for the Gitcoin API client."""
 
 # Valid parameter values as seen at
 # https://github.com/gitcoinco/web/blob/84babc30611c281c817582b4d677dda6366def83/app/dashboard/models.py#L119-L168
@@ -25,24 +26,30 @@ options = {
 }
 
 def _validate_options(field_name, value):
+    """Validate values for the given field name."""
     if value in options[field_name]:
         return value
     msg = 'Unknown value "{val}" for field "{name}".'
     raise ValueError(msg.format(val=value, name=field_name))
 
 def experience_level(value):
+    """Validate values for "experience_level"."""
     return _validate_options('experience_level', value)
 
 def project_length(value):
+    """Validate values for "project_length"."""
     return _validate_options('project_length', value)
 
 def bounty_type(value):
+    """Validate values for "bounty_type"."""
     return _validate_options('bounty_type', value)
 
 def idx_status(value):
+    """Validate values for "idx_status"."""
     return _validate_options('idx_status', value)
 
 def order_by(direction):
+    """Validate values for "order_by"."""
     if direction in options['order_by']:
         return direction
     if direction[0:1] == '-' and direction[1:] in options['order_by']:
