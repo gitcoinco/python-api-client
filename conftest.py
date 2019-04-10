46ec5d27f5aaeea0c1dfa6b5e69ff1d3bab76fd6
binaryflesh <logan.campos123@gmail.com>
2019-04-10 07:45:23 -0500

    fix deprecated pytext fixture syntax
    Signed-off-by: binaryflesh <logan.campos123@gmail.com>

--- conftest.py
+++ conftest.py
@@ -3,4 +3,9 @@ import pytest
 
 # see https://docs.pytest.org/en/latest/example/simple.html#control-skipping-of-tests-according-to-command-line-option
 def pytest_addoption(parser):
-    parser.addoption('--liveapi', action='store_true', default=False, help='run some tests againts live API')
+    parser.addoption('--liveapi', action='store_true', default=False, help='run some tests against live API')
+
+
+@pytest.fixture
+def cmdopt(request):
+    return request.config.getoption("--liveapi")
